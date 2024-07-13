32baf1b91240df63ebc55bb9550199925ff77f3b
Ghislain d'Andlau <gdandlau@gk-software.com>
2024-07-13 11:13:23 +0200

    bits of chap 2

---
+++ src/main/java/com/example/tacocloud/Taco.java
@@ -0,0 +1,21 @@
+package com.example.tacocloud;
+
+import java.util.List;
+// end::allButValidation[]
+
+// tag::allButValidation[]
+import lombok.Data;
+
+@Data
+public class Taco {
+
+    // end::allButValidation[]
+
+    // tag::allButValidation[]
+    private String name;
+    // end::allButValidation[]
+
+    // tag::allButValidation[]
+    private List<String> ingredients;
+
+}
